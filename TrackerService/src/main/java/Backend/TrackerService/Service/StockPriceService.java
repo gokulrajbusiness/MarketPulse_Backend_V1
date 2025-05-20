05244f0949ca12838baddb371a8db94e0a6d07f6
Gokul <rajgokul9909@gmail.com>
2025-05-20 23:16:04 +0530

    Refactoring codebase to run in docker

--- TrackerService/src/main/java/Backend/TrackerService/Service/StockPriceService.java
+++
@@ -1,8 +0,0 @@
-package Backend.TrackerService.Service;
-
-import Backend.TrackerService.Model.StockPrice;
-import reactor.core.publisher.Flux;
-
-public interface StockPriceService {
-    Flux<StockPrice> getPrices();
-}
